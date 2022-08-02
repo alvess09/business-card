983cb2315a75d9fd204f349b3028af5889b16d5e
Ariel Alves <arielalves9@gmail.com>
2022-08-01 23:19:56 -0300

    my first commmit

---
+++ app/src/main/java/com/alvesariel/businesscard/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.alvesariel.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.alvesariel.businesscard.R
+import com.alvesariel.businesscard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
