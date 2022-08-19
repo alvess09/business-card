409585f6fe24b0c64be56b3bd8fcda3b74dbdaa1
Ariel Alves <arielalves9@gmail.com>
2022-08-19 18:46:18 -0300

    add layout addCards

--- app/src/main/java/com/alvesariel/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/alvesariel/businesscard/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import com.alvesariel.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
